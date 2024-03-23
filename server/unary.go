e12f5bb52efbd06bd2d42050aaa3de1bb4abdba1
Ahnaf Tajwear Nabil <ahnafnabil14@gmail.com>
2024-03-24 05:04:14 +0600

    Next Commit

---
+++ server/unary.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	pb "Go-grpc-first/proto"
+	"context"
+)
+
+func (s *helloServer) SayHello(ctx context.Context, req *pb.NoParam) (*pb.HelloResponse, error) {
+	return &pb.HelloResponse{
+		Message: "Hello",
+	}, nil
+}
